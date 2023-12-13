2d78286529091d73f1a0d0f3c27265415096d808
Miguel Domínguez Gómez <100451258@alumnos.uc3m.es>
2023-12-13 16:41:18 +0100

    Actualización: - Añadidas funciones de lectura del input y generación del output - Añadido script de bash para ejecución de la parte 1

---
+++ parte_1/CSP-calls.sh
@@ -0,0 +1,2 @@
+# sh ./parte-1/CSP-calls.sh
+python CSPParking.py ./CSP-tests/parking01
--- parte_1/CSPParking.py
+++ parte_1/CSPParking.py
@@ -1,18 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import random
 import constraint
+
+# ========== LEEMOS LA DIRECCIÓN DEL ARCHIVO ==========
+# La dirección del archivo se pasa como argumento al ejecutar el programa
+# Ejemplo: python CSPParking.py ./CSP-tests/parking01
+
+# Comprobamos que se ha pasado la dirección del archivo
+if len(sys.argv) != 2:
+    print("Error: no se ha pasado la dirección del archivo")
+    sys.exit(1)
+
+# Obtenemos la dirección del archivo
+parking_path = sys.argv[1]
 
 problem = constraint.Problem()
 
-# Tamaño del parking
-dimensiones = (5, 6)
-# Plazas especiales
-plazas_especiales = [(1,1), (1,2), (2,1), (4,1), (5,1), (5, 2)]
+# ========== LEEMOS LOS DATOS DEL FICHERO ==========
+
+"""
+Formato del fichero:
+<filas>x<columnas>
+PE: <posiciones de las plazas especiales separadas por espacios>
+<vehículo 1>
+<vehículo 2>
+...
+
+Formato de los vehículos:
+<id>-<tipo>-<presencia_congelador>
+
+Ejemplo:
+5x6
+PE: (1,1) (1,2) (2,1) (4,1) (5,1) (5,2)
+1-TSU-C
+2-TNU-X
+3-TNU-X
+4-TNU-C
+5-TSU-X
+6-TNU-X
+7-TNU-C
+8-TSU-C
+
+"""
+
+def read_data(path: str) -> tuple:
+    """
+    Lee los datos de un fichero
+    :param path: ruta del fichero
+    :return: tupla con las dimensiones del parking, la lista de plazas especiales y la lista de vehículos
+    """
+    with open(path, "r", encoding="utf8") as file:
+        # Leemos las dimensiones del parking
+        dimensiones = file.readline().split("x")
+        dimensiones = (int(dimensiones[0]), int(dimensiones[1]))
+        # Leemos las plazas especiales
+        plazas_especiales = file.readline()[4:].split(" ")
+        plazas_especiales = [(int(char[1]), int(char[3])) for char in plazas_especiales]
+        # Leemos los vehículos
+        vehiculos = []
+        for line in file:
+            vehiculos.append(line.strip())
+        return dimensiones, plazas_especiales, vehiculos
+
+# Definimos los datos del problema
+dimensiones, plazas_especiales, vehiculos = read_data(parking_path)
+
+# Comprobamos que se han leído bien los datos
+# print(dimensiones)
+# print(plazas_especiales)
+# print(vehiculos)
 
 # NOTA: los índices de las plazas empiezan en 1, no en 0
 
 """
+Ejemplo de parking de tamaño 5x6:
 [1,1] [1,2] (1,3) (1,4) (1,5) (1,6) >>
 [2,1] (2,2) (2,3) (2,4) (2,5) (2,6) >>
 (3,1) (3,2) (3,3) (3,4) (3,5) (3,6) >>
@@ -21,11 +85,15 @@ plazas_especiales = [(1,1), (1,2), (2,1), (4,1), (5,1), (5, 2)]
 
 """
 
+# ========== DEFINIMOS EL DOMINIO DE LAS VARIABLES ==========
+
 # Los vehículos con congelador solo pueden ir en las plazas especiales
 dominio_vehiculos_congelador = plazas_especiales
 
 # El resto de vehículos pueden ir en cualquier plaza
 dominio_vehiculos = [(i, j) for i in range(1, dimensiones[0] + 1) for j in range(1, dimensiones[1] + 1)]
+
+# ========== DEFINIMOS LAS VARIABLES ==========
 
 # Diccionarios para guardar las variables (vehículos)
 # Transporte Sanitario Urgente (TSU): puede no tener congelador (x) o tenerlo (c)
@@ -33,28 +101,30 @@ tsu = {"x": [], "c": []}
 # Transporte Individual no Urgente (TNU): puede no tener congelador (x) o tenerlo (c)
 tnu = {"x": [], "c": []}
 
-# Lista con todos los vehículos
-vehiculos = []
+# Recorremos la lista de vehículos para categorizarlos
+for i in vehiculos:
+    # Separamos los datos del vehículo
+    datos = i.split("-")
+    # Si el vehículo es TSU
+    if datos[1] == "TSU":
+        # Si tiene congelador
+        if datos[2] == "C":
+            tsu["c"].append(i)
+        # Si no tiene congelador
+        else:
+            tsu["x"].append(i)
+    # Si el vehículo es TNU
+    else:
+        # Si tiene congelador
+        if datos[2] == "C":
+            tnu["c"].append(i)
+        # Si no tiene congelador
+        else:
+            tnu["x"].append(i)
 
-# Creamos las variables
-tsu["x"].append("5-TSU-X")
-tsu["c"].append("1-TSU-C")
-tsu["c"].append("8-TSU-C")
-tnu["x"].append("2-TNU-X")
-tnu["x"].append("3-TNU-X")
-tnu["x"].append("6-TNU-X")
-tnu["c"].append("4-TNU-C")
-tnu["c"].append("7-TNU-C")
-
-# Añadimos los vehículos a la lista
-for i in tsu["x"]:
-    vehiculos.append(i)
-for i in tsu["c"]:
-    vehiculos.append(i)
-for i in tnu["x"]:
-    vehiculos.append(i)
-for i in tnu["c"]:
-    vehiculos.append(i)
+# Comprobamos que se han categorizado bien los vehículos
+# print(tsu)
+# print(tnu)
 
 # Añadimos las variables al problema
 for i in tsu["x"]:
@@ -66,13 +136,11 @@ for i in tnu["x"]:
 for i in tnu["c"]:
     problem.addVariable(i, dominio_vehiculos_congelador)
 
-# Restricciones
+# ========== DEFINIMOS LAS RESTRICCIONES ==========
 
 # 1. Todo vehículo tiene que tener asignada una plaza y solo una
-problem.addConstraint(constraint.AllDifferentConstraint())
-
 # 2. Dos vehículos distintos, no pueden ocupar la misma plaza
-# La anterior restricción ya lo impone
+problem.addConstraint(constraint.AllDifferentConstraint())
 
 # 3. Los vehículos con congelador solo pueden ir en las plazas especiales
 # Esto ya se ha tenido en cuenta al definir el dominio de las variables
@@ -148,9 +216,71 @@ problem.addConstraint(maniobrabilidad, vehiculos)
 solutions = problem.getSolutions()
 
 # Computamos una solución aleatoria
-solution = problem.getSolution()
+# solution = problem.getSolution()
 
-print(solution)
-print(len(solutions))
+# print(solution)
+# print(len(solutions))
 
-# TODO: Leer datos de un fichero y escribir la solución en otro fichero
+# ========== GENERAMOS EL FICHERO DE SALIDA ==========
+"""
+Formato del fichero:
+"N. Sol:", <número de soluciones>
+<solución 1>
+
+<solución 2>
+
+...
+
+Formato de la solución:
+tablero en formato de matriz csv
+Si la plaza está vacía, se escribe un "-"
+Si la plaza está ocupada, se escribe "<id>-<tipo>-<presencia_congelador>"
+
+Ejemplo:
+"N. Sol:",302
+"7-TNU-C","1-TSU-C","5-TSU-X","-","-","-"
+"-","-","-","-","-","-"
+"2-TNU-X","3-TNU-X","6-TNU-X","-","-","-"
+"-","-","-","-","-","-"
+"4-TNU-C","8-TSU-C","-","-","-","-"
+"""
+
+def write_output(path: str, solutions: list, n_print: int):
+    """
+    Escribe las soluciones en un fichero
+    :param path: ruta del fichero
+    :param solutions: lista de soluciones
+    """
+    with open(path, "w", encoding="utf8") as file:
+        # Escribimos el número de soluciones
+        file.write("\"N. Sol:\"," + str(len(solutions)) + "\n")
+        # Seleccionamos n_print soluciones aleatorias
+        solutions = random.sample(solutions, n_print)
+        # Escribimos las soluciones
+        for i in solutions:
+            # i es un diccionario: las claves son los vehículos y los valores son las posiciones
+            for j in range(1, dimensiones[0] + 1):
+                for k in range(1, dimensiones[1] + 1):
+                    # Si la plaza está vacía
+                    if (j, k) not in i.values():
+                        file.write("\"-\"")
+                    # Si la plaza está ocupada
+                    else:
+                        # Recorremos los vehículos
+                        for l in i.keys():
+                            # Si la posición del vehículo coincide con la posición actual
+                            if i[l] == (j, k):
+                                # Escribimos el vehículo
+                                file.write("\"" + l + "\"")
+                    # Si no es la última plaza de la fila
+                    if k != dimensiones[1]:
+                        file.write(",")
+                # Si no es la última fila
+                if j != dimensiones[0]:
+                    file.write("\n")
+            # Si no es la última solución
+            if i != solutions[-1]:
+                file.write("\n\n")
+            
+# Escribimos las soluciones en el fichero
+write_output(parking_path + ".csv", solutions, 10)
