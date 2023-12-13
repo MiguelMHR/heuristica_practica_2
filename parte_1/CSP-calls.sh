3891c80e7fe6a15d39fb396fe4f7289fa5e7301d
Miguel Domínguez Gómez <100451258@alumnos.uc3m.es>
2023-12-13 17:04:15 +0100

    Añadido test 2

--- parte_1/CSP-calls.sh
+++ parte_1/CSP-calls.sh
@@ -1,2 +1,8 @@
+# FORMA DE EJECUTAR EL SCRIPT EN TERMINAL:
 # sh ./parte-1/CSP-calls.sh
+
+##### EJECUCIÓN DEL PRIMER TEST -> EJEMPLO DEL ENUNCIADO #####
 python CSPParking.py ./CSP-tests/parking01
+
+##### EJECUCIÓN DEL SEGUNDO TEST -> CASO IMPOSIBLE - NO HAY PLAZAS ESPECIALES #####
+python CSPParking.py ./CSP-tests/parking02
