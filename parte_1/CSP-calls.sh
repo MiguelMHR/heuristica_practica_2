e940b9371e7edde4d2d8710c2b40c54b888283f6
MiguelMHR <100451258@alumnos.uc3m.es>
2023-12-14 12:05:04 +0100

    Comentario añadido

--- parte_1/CSP-calls.sh
+++ parte_1/CSP-calls.sh
@@ -1,32 +1,34 @@
 # FORMA DE EJECUTAR EL SCRIPT EN TERMINAL:
 # sh ./parte-1/CSP-calls.sh
 
+# NOTA: los primeros tests pueden tardar un poco en ejecutarse, ya que poseen un gran número de soluciones
+
 ##### EJECUCIÓN DEL PRIMER TEST -> EJEMPLO DEL ENUNCIADO #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking01
+python parte_1/CSPParking.py parte_1/CSP-tests/parking01
 
 ##### EJECUCIÓN DEL SEGUNDO TEST -> CASO POSIBLE - TODO SON PLAZAS ESPECIALES #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking02
+python parte_1/CSPParking.py parte_1/CSP-tests/parking02
 
 ##### EJECUCIÓN DEL TERCER TEST -> CASO POSIBLE - HAY MAS TNUS QUE Nº COLUMNAS #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking03
+python parte_1/CSPParking.py parte_1/CSP-tests/parking03
 
 ##### EJECUCIÓN DEL CUARTO TEST -> CASO POSIBLE - HAY MAS TSUS QUE TNUS #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking04
+python parte_1/CSPParking.py parte_1/CSP-tests/parking04
 
 ##### EJECUCIÓN DEL QUINTO TEST -> CASO POSIBLE - HAY MISMOS TSUS QUE TNUS #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking05
+python parte_1/CSPParking.py parte_1/CSP-tests/parking05
 
 ##### EJECUCIÓN DEL SEXTO TEST -> CASO POSIBLE - HAY MENOS TSUS QUE TNUS #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking06
+python parte_1/CSPParking.py parte_1/CSP-tests/parking06
 
 ##### EJECUCIÓN DEL SÉPTIMO TEST -> CASO IMPOSIBLE - NO HAY PLAZAS ESPECIALES #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking07
+python parte_1/CSPParking.py parte_1/CSP-tests/parking07
 
 ##### EJECUCIÓN DEL OCTAVO TEST -> CASO IMPOSIBLE - TSU CON TNU A LA DCHA #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking08
+python parte_1/CSPParking.py parte_1/CSP-tests/parking08
 
 ##### EJECUCIÓN DEL NOVENO TEST -> CASO IMPOSIBLE - MÁS VEHÍCULOS QUE HUECOS #####
-#python parte_1/CSPParking.py parte_1/CSP-tests/parking09
+python parte_1/CSPParking.py parte_1/CSP-tests/parking09
 
 ##### EJECUCIÓN DEL DÉCIMO TEST -> CASO IMPOSIBLE - VEHÍCULOS JUNTOS POR TENER CONGELADOR #####
 python parte_1/CSPParking.py parte_1/CSP-tests/parking10
